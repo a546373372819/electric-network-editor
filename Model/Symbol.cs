43722c95bd1c938c1fd649f88f62eee6b36ec98b
Borislav Čelar <bora.celar@gmail.com>
2025-02-14 02:09:17 +0100

    symbol model

---
+++ Model/Symbol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace electric_network_editor.Model
+{
+    enum SymbolType
+    {
+        Source,
+        Switch,
+        Node
+    }
+    internal class Symbol
+    {
+    }
+}
