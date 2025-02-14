b100e3887b0365d82125657294aa9d422b3c92bc
Borislav Čelar <bora.celar@gmail.com>
2025-02-14 03:45:20 +0100

    Line drawing between symbols

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using electric_network_editor.Model;
+using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +24,26 @@ namespace electric_network_editor
     {
 
         private RadioButton _selectedSymbolBtn;
+        private UIElement _selectedSymbol;
+        private List<Symbol> _drawnSymbols=new List<Symbol>();
+
+
+        Dictionary<UIElement, Symbol> _UIElementToSymbolMap = new Dictionary<UIElement, Symbol>();
+
+        Dictionary<SymbolType, string> _symbolImgSrcMap= new Dictionary<SymbolType, string>
+
+        { 
+            { SymbolType.Source,"Images/triangle.png" },
+            { SymbolType.Switch,"Images/rectangle.png" },
+            { SymbolType.Node,"Images/circle.png" },
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateSymbolButtons();
+
         }
 
 
@@ -36,12 +55,27 @@ namespace electric_network_editor
             }
         }
 
-        private void NodeConnectButton_Click(object sender, RoutedEventArgs e)
+
+        private void SymbolIcon_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            Console.WriteLine("AAA");
+            if ((bool)NetworkConnectBtn.IsChecked)
+            {
+                
+                if(_selectedSymbol != null) 
+                {
 
+                    DrawLine(_selectedSymbol, (UIElement)sender);
+                    RemoveHighlight(_selectedSymbol);
+                    _selectedSymbol = null;
+                    return;
+                }
+
+                _selectedSymbol = (UIElement)sender;
+                ApplyHighlight(_selectedSymbol);
+
+            }
         }
-
-
 
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -49,30 +83,142 @@ namespace electric_network_editor
             {
                 Point clickPosition = e.GetPosition(NetworkCanvas);
 
-                PlaceSymbol(clickPosition);
+                DrawSymbol(clickPosition);
 
             }
         }
 
-        private void PlaceSymbol(Point position)
+        private void ConnectionBtn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            RemoveHighlight(_selectedSymbol);
+            _selectedSymbol = null;
+        }
+
+        private void DrawLine(UIElement parent,UIElement child)
         {
 
-            Console.WriteLine("aa");
-            string ImgPath = _selectedSymbolBtn.Tag.ToString();
+            double x1 = _UIElementToSymbolMap[parent].position.X + 50;
+            double y1= _UIElementToSymbolMap[parent].position.Y + 50;
+
+            double x2= _UIElementToSymbolMap[child].position.X + 50;
+            double y2= _UIElementToSymbolMap[child].position.Y + 50;
+
+            if(Math.Abs(x2-x1)>50 & Math.Abs(y2 - y1)>50)
+            {
+                Line connectionLineVert = new()
+                {
+                    X1 = x1,
+                    Y1 = y1,
+
+                    X2 = x1,
+                    Y2 = y2,
+
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 5
+                };
+
+                Line connectionLineHor = new()
+                {
+                    X1 = x1,
+                    Y1 = y2,
+
+                    X2 = x2,
+                    Y2 = y2,
+
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 5
+                };
+
+                NetworkCanvas.Children.Add(connectionLineHor);
+                NetworkCanvas.Children.Add(connectionLineVert);
+
+                return;
+            }
+
+            Line connectionLine = new()
+            {
+                X1 = x1,
+                Y1 = y1,
+
+                X2 = x2,
+                Y2 = y2,
+
+                Stroke = Brushes.Black,
+                StrokeThickness = 5
+            };
+
+            NetworkCanvas.Children.Add(connectionLine);
+        }
+
+        private void DrawSymbol(Point position)
+        {
+           
+            SymbolType drawnSymbolType = (SymbolType)_selectedSymbolBtn.Tag;
 
             Image symbolImage = new Image
             {
-                Source = new BitmapImage(new System.Uri(ImgPath, System.UriKind.Relative)),
+                Source = new BitmapImage(new System.Uri(_symbolImgSrcMap[drawnSymbolType], System.UriKind.Relative)),
                 Width = 100,
                 Height = 100
             };
+            symbolImage.MouseDown += SymbolIcon_MouseDown;
 
-            Canvas.SetLeft(symbolImage, position.X - 50);
-            Canvas.SetTop(symbolImage, position.Y - 50);
+            position.X -= 50;
+            position.Y -= 50;
+            Canvas.SetLeft(symbolImage, position.X);
+            Canvas.SetTop(symbolImage, position.Y);
+
+            Symbol drawnSymbol = new Symbol(drawnSymbolType,new List<Symbol>(),position);
+
+            _drawnSymbols.Add(drawnSymbol);
+            _UIElementToSymbolMap.Add(symbolImage, drawnSymbol);
 
             NetworkCanvas.Children.Add(symbolImage);
+            
             _selectedSymbolBtn.IsChecked = false;
             _selectedSymbolBtn = null;
         }
+
+        private void CreateSymbolButtons()
+        {
+            foreach (SymbolType obj in Enum.GetValues(typeof(SymbolType)))
+            {
+                RadioButton rb = new RadioButton();
+                rb.Click += new RoutedEventHandler(SymbolButton_Click);
+                rb.Tag= obj;
+
+                Image symbolImage = new Image
+                {
+                    Source = new BitmapImage(new System.Uri(_symbolImgSrcMap[obj], System.UriKind.Relative)),
+                };
+
+                rb.Content = symbolImage;
+
+                Sidebar.Children.Add(rb);
+
+            }
+        }
+
+        private void ApplyHighlight(UIElement element)
+        {
+            if (element is Image img)
+            {
+                img.Effect = new System.Windows.Media.Effects.DropShadowEffect
+                {
+                    Color = Colors.Yellow,
+                    BlurRadius = 10,
+                    ShadowDepth = 0
+                };
+            }
+        }
+
+        private void RemoveHighlight(UIElement element)
+        {
+            if (element is Image img)
+            {
+                img.Effect = null; // Remove glow effect
+            }
+        }
+
     }
 }
--- Model/Symbol.cs
+++ Model/Symbol.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.TextFormatting;
 
 namespace electric_network_editor.Model
 {
@@ -14,5 +16,18 @@ namespace electric_network_editor.Model
     }
     internal class Symbol
     {
+        int id;
+        SymbolType type;
+        List<Symbol> children;
+        public Point position { get; set; }
+
+
+        public Symbol(SymbolType type, List<Symbol> children, Point position)
+        {
+            this.id = DateTime.Now.Second;
+            this.type = type;
+            this.children = children;
+            this.position = position;
+        }
     }
 }
