cf7c357185612e26c33651fe475a487967440b99
Borislav Čelar <bora.celar@gmail.com>
2025-02-27 22:56:49 +0100

    .

---
+++ electric-network-editor/CanvasExportProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition.Hosting.Core;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace electric_network_editor
+{
+    internal class CanvasExportProvider : ExportDescriptorProvider
+    {
+        private readonly Canvas _canvas;
+
+        public CanvasExportProvider(Canvas canvas)
+        {
+            _canvas = canvas;
+        }
+
+        public override IEnumerable<ExportDescriptorPromise> GetExportDescriptors(
+            CompositionContract contract, DependencyAccessor descriptorAccessor)
+        {
+            if (contract.ContractType == typeof(Canvas))
+            {
+                yield return new ExportDescriptorPromise(
+                    contract,
+                    "Canvas Export",
+                    true, // No dependencies
+                    () => Array.Empty<CompositionDependency>(),
+                    dependencies =>
+                    {
+                        return ExportDescriptor.Create((c, o) => _canvas, new Dictionary<string, object>());
+                    });
+            }
+        }
+    }
+}
