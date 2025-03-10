1cdf30c0e98d8ef8b27c82e78e71305806aa1eb1
Borislav Čelar <bora.celar@gmail.com>
2025-03-10 01:39:00 +0100

    refactored source node command

---
+++ PluginContracts/CommandButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PluginContracts
+{
+    public class CommandButton
+    {
+
+        public RadioButton button;
+
+        public CommandButton(Image i, INetworkCanvasStrategy s)
+        {
+
+        }
+    }
+}
---
+++ PluginContracts/Symbol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+
+namespace PluginContracts
+{
+
+    public abstract class Symbol
+    {
+        int Id;
+        List<Symbol>? Children;
+        public Point Position { get; }
+        public abstract string ImgSrc { get; }
+
+        protected Symbol(int id, Point position)
+        {
+            this.Id = id;
+            this.Children = new List<Symbol>();
+            this.Position = position;
+
+
+        }
+    }
+}
